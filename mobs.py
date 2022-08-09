d022b4c459c58609ad5c535b5a6fcc98927da072
fantasticsquirrel <fantasticsquirrel@pm.me>
2022-08-08 21:21:39 -0500

    initial change_stats function

--- mobs.py
+++ mobs.py
@@ -1,12 +1,14 @@
 metadata = Hash()
 mobdata = Hash()
 moblocations = Hash()
+moblocationcounts = Hash()
 random.seed()
 
 @construct
 def seed():
     metadata['operator'] = ctx.caller
     metadata['map_contract'] = 'con_rpg_map'
+    metadata['c_list'] = ["con_battles"]
 
 @export
 def change_metadata(key: str, new_value: str):
@@ -34,13 +36,11 @@ def new_mob_apply_stats(type):
 
     stats = mobdata[type]
 
-    mobdata[type, count, "stats"]={
-        "MS": stats["MS"],
-        "MD": stats["MD"],
-        "RS": stats["RS"],
-        "RD": stats["RD"],
-        "HP": stats["HP"]
-    }
+    mobdata[type, count, "stats", "MS"] = stats["MS"]
+    mobdata[type, count, "stats", "MD"] = stats["MD"]
+    mobdata[type, count, "stats", "RS"] = stats["RS"]
+    mobdata[type, count, "stats", "RD"] = stats["RD"]
+    mobdata[type, count, "stats", "HP"] = stats["HP"]
 
     mobdata[type, count, "alive"] = True
     mobdata[type,"count"] = count
@@ -57,11 +57,12 @@ def new_mob_random(type : str):
     row = random.randint(0, map['maxrc'])
     column = random.randint(0, map['maxrc'])
 
-    moblocations[row , column , ct[0] , ct[1]] = True
-    if moblocations[row , column , "count"] == None:
-        moblocations[row , column , "count"] =1
+    moblocations[ct[0] , ct[1]] = [row , column]
+
+    if moblocationcounts[row , column] == None:
+        moblocationcounts[row , column] =1
     else:
-        moblocations[row , column , "count"] +=1
+        moblocationcounts[row , column] +=1
 
 @export
 def lotsa_mobs(type : str , num : int):
@@ -69,3 +70,17 @@ def lotsa_mobs(type : str , num : int):
     while m <= num:
         new_mob_random (type)
         m+=1
+
+@export
+def change_stats(type : str, count : int, stat : str, amount : int): #this is a simple function to change the stats of a mob. the conditions to change the stat will come from the functions or contracts calling it
+    assert ctx.caller in metadata['c_list'], "You can't change stats."
+    mobstat = mobdata[type, count, "stats", stat] += amount
+    mobstat += amount
+    if stat == "HP" and mobstat <= 0:
+        amount = 0
+        mobdata[type, count, "alive"] = False
+        loc = moblocations[type , count]
+        moblocations[type , count] = False
+        moblocationcounts[loc[0] , loc[1]] -=1
+
+    mobdata[type, count, "stats", stat] = amount
